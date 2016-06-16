d292e1f3afd35918db47036e7ec723b27b6548b6
Austreng <kolbjora@stud.ntnu.no>
2016-06-16 17:18:41 +0200

    Made interactive CLI debugging mode
    Signed-off-by: Austreng <kolbjora@stud.ntnu.no>

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 #include <chrono>
 
 namespace{
-  const int height = 10; //Y mines
-  const int width = 25; //X mines
+  const int height = 5; //Y mines
+  const int width = 5; //X mines
+  const int mines = 10;
   const int tileSize = 40; //Pixels
   const int borderSize = 2; //Pixels in border
   const sf::Color openColor(120, 120, 120);
@@ -14,35 +15,22 @@ namespace{
 }
 
 int main(){
-  sf::RenderWindow window(sf::VideoMode(width*tileSize + borderSize, height*tileSize + borderSize),
-  "This is a nice window", sf::Style::Titlebar);
+  Field board(height, width, mines);
 
-  Field *field = new Field(height, width, 5);
+  std::cout << "Before mine placement" << std::endl;
+  std::cout << board << std::endl;
 
-  while(window.isOpen()){
-    window.clear();
-    for(int y = height - 1; y >= 0; y--){
-      for(int x = 0; x < width; x++){
-        std::this_thread::sleep_for(std::chrono::milliseconds(30));
-        field->toggleOpen(x, y);
-    /// *** ///
-        for(int i = 0; i < height; i++){
-          for(int j = 0; j < width; j++){
-            const int tileX = tileSize * j;
-            const int tileY = tileSize * (height - i - 1); //So memory (x,y) == graphical (x,y)
-            sf::RectangleShape tile;
-            tile.setSize(sf::Vector2f(tileSize - borderSize, tileSize - borderSize));
-            tile.setFillColor(field->isOpen(j, i) ? openColor : closedColor);
-            tile.setPosition(tileX + borderSize, tileY + borderSize);
-            window.draw(tile);
-          }
-        }
-    /// *** ///
-    window.display();
-  }}
-  //
+  std::cout << "Opening (0, 0)..." << std::endl;
+  board.toggleOpen(0, 0);
+  board.placeMines(0, 0);
+  std::cout << board << std::endl;
+
+  while(true){
+    int x, y;
+    std::cout << "(x, y) : ";
+    std::cin >> x >> y;
+    board.toggleOpen(x, y);
+    std::cout << board << std::endl;
   }
-  delete[] field;
-  field = nullptr;
   return 0;
 }
