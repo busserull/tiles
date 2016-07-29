e0ba1867bc331d8ad784c97034014aa3afee4bbf
Austreng <kolbjora@stud.ntnu.no>
2016-07-29 22:57:13 +0200

    Changed int to sf::Uint32 in receive
    Signed-off-by: Austreng <kolbjora@stud.ntnu.no>

--- client.cpp
+++ client.cpp
@@ -1,5 +1,7 @@
 #include "client.hpp"
 
+#include <iostream>
+
 Client::Client() : ipAddress("127.0.0.1"), port(55001){
 }
 
@@ -23,23 +25,29 @@ void Client::connectToHost(std::string playerName){
 
 void Client::receiveCompleteBoard(Field& field){
   sf::Packet packet;
+  socket.receive(packet);
   // Check message
   {
     std::string message;
     packet >> message;
+    std::cout << message << std::endl; ///
     if(message != "completeBoard"){
       throw message;
     }
   }
   // Height, width and mines
-  int height, width, mines;
+  sf::Uint32 height, width, mines;
   packet >> height >> width >> mines;
+  std::cout << "height: " << height << std::endl;
+  std::cout << "width: " << width << std::endl;
+  std::cout << "mines: " << mines << std::endl;
   field = Field(height, width, mines);
   int tileCount = height * width;
   for(int i = 0; i < tileCount; i++){
-    int x, y;
+    sf::Uint32 x, y;
     bool isOpen, isMine, isFlagged;
     std::string flagger;
+    std::cout << x << ", " << y << std::endl; ///
     packet >> x >> y >> isOpen >> isMine >> isFlagged >> flagger;
     field.setTile(x, y, isOpen, isMine, isFlagged, flagger);
   }
